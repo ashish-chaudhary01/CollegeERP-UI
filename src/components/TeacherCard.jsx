28cf693bbca6575d4545bd6cb8d79860444f0d50
Ashish Chaudhary <sk8896107@gmail.com>
2026-09-13 11:46:36 +0530

    implement teacher management with search functionality and display in Teachers component

---
+++ src/components/TeacherCard.jsx
@@ -0,0 +1,42 @@
+function TeacherCard({ teacher }) {
+  const image_url = teacher.profilePictureUrl || "/no-image.jpg";
+  return (
+    <div className="relative rounded-lg border border-black/20 shadow-md hover:shadow-lg w-full h-full hover:-translate-y-1.5 duration-300 ease-out flex flex-col justify-between overflow-hidden">
+      {/* role */}
+      <p className="absolute right-2 top-2 text-[8px] rounded-full mb-2 px-2 py-1 uppercase tracking-[0.30em] bg-purple-500/30 text-purple-700 inline font-bold">
+        teacher
+      </p>
+      <div className="aspect-square">
+        <img
+          src={image_url}
+          alt="student-image"
+          className="w-full h-full object-cover  rounded-t-lg"
+        />
+      </div>
+      {/* details */}
+      <div className="px-2 pb-2 flex-1">
+        <h2 className="mt-2 font-bold text-xl capitalize leading-tight">
+          {teacher.userId.name}
+        </h2>
+        <p className="text-[13px] text-slate-500">{teacher.userId.email}</p>
+        <div className="mt-1 text-xs text-gray-700 flex gap-1 flex-wrap">
+          <span
+            className={`px-2 py-1 rounded-full backdrop-blur-2xl border-black/10 border  ${teacher.status === "active" ? "bg-green-400/20 text-green-700" : "bg-red-400/20 text-red-700"}`}
+          >
+            Status : {teacher.status}
+          </span>
+        </div>
+        {/* other details */}
+        <div className="mt-2 text-sm">
+          <p className="">Department : {teacher.department.departmentName}</p>
+        </div>
+      </div>
+      {/* view profile button */}
+      <button className="w-full p-2 bg-blue-600 text-white font-bold text-md rounded-b-lg cursor-pointer hover:bg-blue-700">
+        View Profile
+      </button>
+    </div>
+  );
+}
+
+export default TeacherCard;
--- src/pages/admin/Teachers.jsx
+++ src/pages/admin/Teachers.jsx
@@ -1,5 +1,75 @@
+import { useEffect, useState } from "react";
+import TeacherCard from "../../components/TeacherCard";
+
 const Teachers = () => {
-  return <div>Teachers</div>;
+  const [inputSearch, setInputSearch] = useState("");
+  const [teachers, setTeachers] = useState([]);
+  useEffect(() => {
+    async function fetchTeachers() {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}/admin/teachers`,
+          { method: "GET", credentials: "include" },
+        );
+
+        const data = await res.json();
+        setTeachers(data.teacher);
+        console.log(data.teacher);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
+    fetchTeachers();
+  }, []);
+
+  const filteredTeachers =
+    inputSearch.trim().length > 0
+      ? teachers.filter((teacher) => {
+          const teacherName = teacher?.userId?.name?.toLowerCase() ?? "";
+          const teacherEmail = teacher?.userId?.email?.toLowerCase() ?? "";
+          const searchTerm = inputSearch.trim().toLowerCase();
+          return (
+            teacherName.includes(searchTerm) ||
+            teacherEmail.includes(searchTerm)
+          );
+        })
+      : teachers;
+
+  return (
+    <div className="min-h-screen overflow-hidden">
+      {/* heading */}
+      <h1 className="text-4xl font-bold">Teachers</h1>
+      <p className="text-gray-400 text-[13px]">
+        Search, view and manage Teacher records
+      </p>
+
+      {/* search bar */}
+      <div className="flex items-center mt-4 gap-2">
+        <div className="flex items-center flex-1">
+          <input
+            type="text"
+            value={inputSearch}
+            onChange={(e) => setInputSearch(e.target.value)}
+            placeholder="Search by name or email..."
+            className="outline-0 border-black/15 px-4 py-2 rounded bg-gray-200 placeholder:text-sm flex-1 text-md text-slate-700"
+          />
+        </div>
+
+        <div className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded font-semibold shadow hover:bg-blue-700/80 duration-200 cursor-pointer">
+          <span>+</span>
+          <span className="hidden lg:block">Add Teachers</span>
+        </div>
+      </div>
+
+      {/* student grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-3 sm:gap-4 items-stretch md:p-4 py-4">
+        {filteredTeachers?.map((teacher, idx) => (
+          <TeacherCard teacher={teacher} key={idx} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Teachers;
