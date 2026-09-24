b633f89a181b7848c2bba612ca5cedf096b9fd5c
Ashish Chaudhary <sk8896107@gmail.com>
2026-09-24 21:39:20 +0530

    improved forgotPassword page

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -3,19 +3,26 @@ import { useState } from "react";
 import { Link } from "react-router";
 
 function ForgotPassword() {
-  const [email, setEmail] = useState("assiku@gmail.com");
+  const [email, setEmail] = useState("");
   const [otp, setOtp] = useState("");
-  const [emailVerifying, setEmailVerifying] = useState(false);
   const [emailVerified, setEmailVerified] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [otpVerified, setOtpVerified] = useState(false);
   const [error, setError] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [resetToken, setResetToken] = useState("");
 
   //   verify email
   const handleForgotPassword = async () => {
     try {
-      setEmailVerifying(true);
+      setLoading(true);
       setError("");
+      if (email === "") {
+        throw new Error("Email is empty");
+      }
       const res = await fetch(
-        `${import.meta.env.VITE_API_URL}/auth/reset-password`,
+        `${import.meta.env.VITE_API_URL}/auth/forgot-password`,
         {
           method: "POST",
           headers: {
@@ -35,12 +42,73 @@ function ForgotPassword() {
     } catch (error) {
       setError(error.message || "Unable to reset password try again.");
     } finally {
-      setEmailVerifying(false);
+      setLoading(false);
     }
   };
 
   //   verify otp
-  const handleVerifyOtp = async () => {};
+  const handleVerifyOtp = async () => {
+    try {
+      setLoading(true);
+      setError("");
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/auth/verify-otp`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({ email, otp }),
+        },
+      );
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data.message || data.error || "failed to verify otp");
+      }
+      setResetToken(data.resetToken);
+      setOtpVerified(true);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // reset password
+  const handleResetPassword = async () => {
+    try {
+      if (password === "" || confirmPassword === "" || password.length < 8) {
+        throw new Error("Password must be of 8 characters");
+      }
+      if (password !== confirmPassword)
+        throw new Error("confirm password do not match");
+      setLoading(true);
+      setError("");
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/auth/reset-password`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({ resetToken, password, confirmPassword }),
+        },
+      );
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(
+          data.message || data.error || "unable to reset password",
+        );
+      }
+    } catch (error) {
+      setError("unable to reset password");
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="flex pt-20 sm:pt-32 md:pt-40 justify-center w-full h-screen">
       <div className="max-w-md p-4 w-full">
@@ -52,10 +120,16 @@ function ForgotPassword() {
           />
         </div>
         <h2 className="text-2xl font-bold tracking-wide text-center">
-          {emailVerified ? "Password Reset" : "Forgot Password?"}
+          {otpVerified
+            ? "Set new password"
+            : emailVerified
+              ? "Password Reset"
+              : "Forgot Password?"}
         </h2>
         <p className="mt-1 text-sm text-gray-700 text-center font-medium">
-          {emailVerified ? (
+          {otpVerified ? (
+            "choose a strong password"
+          ) : emailVerified ? (
             <span>
               OTP is sent to{" "}
               <span className="font-semibold text-slate-800">{email}</span>
@@ -66,36 +140,89 @@ function ForgotPassword() {
         </p>
 
         <div className="mt-6">
-          <label
-            htmlFor={emailVerified ? "otp" : "email"}
-            className="block font-semibold text-xs text-slate-500"
-          >
-            {emailVerified ? "Enter OTP" : "Email"}
-          </label>
-          <input
-            type={emailVerified ? "text" : "email"}
-            name={emailVerified ? "otp" : "email"}
-            value={emailVerified ? otp : email}
-            onChange={(e) => {
-              emailVerified ? setOtp(e.target.value) : setEmail(e.target.value);
-              setError("");
-            }}
-            placeholder={emailVerified ? "Enter otp" : "Enter your email"}
-            className="mt-1 w-full px-4 py-3 border border-slate-300 focus:border-indigo-500 duration-150 rounded-lg placeholder:text-sm outline-0 text-sm"
-          />
-          {/*  button */}
-          <button
-            onClick={emailVerified ? handleVerifyOtp : handleForgotPassword}
-            disabled={emailVerifying}
-            className="mt-4 w-full py-2 bg-blue-700 rounded text-white font-semibold text-md shadow hover:bg-blue-600 duration-200"
-          >
-            {emailVerifying
-              ? "Verfying email..."
-              : emailVerified
-                ? "Verify OTP"
-                : "Reset Password"}
-          </button>
-          {error && <p className="mt-2 text-sm text-red-500">{error}</p>}
+          {otpVerified ? (
+            <>
+              <div>
+                <label
+                  htmlFor="newPassword"
+                  className="block font-semibold text-xs text-slate-500"
+                >
+                  Password
+                </label>
+                <input
+                  type="text"
+                  placeholder=""
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  name="newPassword"
+                  className="mt-1 w-full px-4 py-3 border border-slate-300 focus:border-indigo-500 duration-150 rounded-lg placeholder:text-sm outline-0 text-sm"
+                />
+              </div>
+              <div className="mt-4">
+                <label
+                  htmlFor="confirmPassword"
+                  className="block font-semibold text-xs text-slate-500"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="text"
+                  placeholder=""
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  name="confirmPassword"
+                  className="mt-1 w-full px-4 py-3 border border-slate-300 focus:border-indigo-500 duration-150 rounded-lg placeholder:text-sm outline-0 text-sm"
+                />
+              </div>
+              {/* button */}
+              <button
+                onClick={handleResetPassword}
+                disabled={loading}
+                className="mt-4 w-full py-2 bg-blue-700 rounded text-white font-semibold text-md shadow hover:bg-blue-600 duration-200"
+              >
+                {loading ? "Reseting Password..." : "Reset Password"}
+              </button>
+              {error && <p className="mt-2 text-sm text-red-500">{error}</p>}
+            </>
+          ) : (
+            <>
+              <label
+                htmlFor={emailVerified ? "otp" : "email"}
+                className="block font-semibold text-xs text-slate-500"
+              >
+                {emailVerified ? "Enter OTP" : "Email"}
+              </label>
+              <input
+                type={emailVerified ? "text" : "email"}
+                name={emailVerified ? "otp" : "email"}
+                value={emailVerified ? otp : email}
+                required
+                onChange={(e) => {
+                  emailVerified
+                    ? setOtp(e.target.value)
+                    : setEmail(e.target.value.toLowerCase());
+                  setError("");
+                }}
+                placeholder={emailVerified ? "Enter otp" : "Enter your email"}
+                className="mt-1 w-full px-4 py-3 border border-slate-300 focus:border-indigo-500 duration-150 rounded-lg placeholder:text-sm outline-0 text-sm"
+              />
+              {/*  button */}
+              <button
+                onClick={emailVerified ? handleVerifyOtp : handleForgotPassword}
+                disabled={loading}
+                className="mt-4 w-full py-2 bg-blue-700 rounded text-white font-semibold text-md shadow hover:bg-blue-600 duration-200"
+              >
+                {loading
+                  ? emailVerified
+                    ? "Verifying OTP..."
+                    : "Sending OTP..."
+                  : emailVerified
+                    ? "Verify OTP"
+                    : "Send OTP"}
+              </button>
+              {error && <p className="mt-2 text-sm text-red-500">{error}</p>}
+            </>
+          )}
 
           <Link
             to="/"
